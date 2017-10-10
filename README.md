9c726a01aa3bede3888cce331c93ad3a8ded49f5
rdu <ruixiang.du@gmail.com>
2017-10-10 15:14:48 -0400

    selected part of librav by rdu as code base of acel

---
+++ README.md
@@ -0,0 +1,2 @@
+# ACEL Code Base
+
---
+++ src/lcmtypes/c/lcmtypes/acel_lcm_msgs.h
@@ -0,0 +1,25 @@
+#ifndef __lcmtypes_acel_lcm_msgs_h__
+#define __lcmtypes_acel_lcm_msgs_h__
+
+#include "acel_lcm_msgs_Edge_t.h"
+#include "acel_lcm_msgs_Octomap_t.h"
+#include "acel_lcm_msgs_Graph_t.h"
+#include "acel_lcm_msgs_Pose_t.h"
+#include "acel_lcm_msgs_WayPoint_t.h"
+#include "acel_lcm_msgs_UAVTrajectoryPoint_t.h"
+#include "acel_lcm_msgs_LaserScanPoints_t.h"
+#include "acel_lcm_msgs_PolyCurveSegment_t.h"
+#include "acel_lcm_msgs_TimeStamp_t.h"
+#include "acel_lcm_msgs_NewDataReady_t.h"
+#include "acel_lcm_msgs_MissionInfo_t.h"
+#include "acel_lcm_msgs_example_t.h"
+#include "acel_lcm_msgs_QuadrotorTransform.h"
+#include "acel_lcm_msgs_KeyframeSet_t.h"
+#include "acel_lcm_msgs_Point3Df_t.h"
+#include "acel_lcm_msgs_PolynomialCurve_t.h"
+#include "acel_lcm_msgs_UAVTrajectory_t.h"
+#include "acel_lcm_msgs_Path_t.h"
+#include "acel_lcm_msgs_Vertex_t.h"
+#include "acel_lcm_msgs_Keyframe_t.h"
+
+#endif
